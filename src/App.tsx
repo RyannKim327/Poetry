a91ee5f4561ca2805ba88fef5279f467952441a9
RyannKim327 <rksesgundo123@gmail.com>
2025-06-17 07:36:39 +0800

    06-17-25 07:36

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       );
       const d = JSON.parse(data.files["poetry.json"].content);
       setPoems(d.reverse());
-      setDefaultList(d.reverse());
+      setDefaultList(d);
     })();
   }, []);
 
@@ -87,7 +87,7 @@ function App() {
                 onClick={() => {
                   setPoem(poems[i]);
                 }}
-                className="bg-[#F5ECD9] p-[3px] rounded cursor-pointer border-[#3B2F2F] border-1 border-solid"
+                className={`${p.title.charCodeAt(0) > 255 ? "baybayin" : "font-serif"} bg-[#F5ECD9] p-[3px] rounded cursor-pointer border-[#3B2F2F] border-1 border-solid`}
               >
                 {p.title}
               </div>
@@ -98,7 +98,7 @@ function App() {
         </div>
       </div>
       <div className="flex flex-col border-[#3B2F2F] rounded justify-start items-center w-full bg-[#F5ECD9] m-2 box-border shadow-[0_0_10px_#A58D7F]">
-        <p className="text-[#3B2F2F] text-lg md:text-5xl font-serif pb-4 md:pb-6">
+        <p className={`${poem?.title.charCodeAt(0) || 0 > 255 ? "baybayin" : "font-serif"} text-[#3B2F2F] text-lg md:text-5xl pb-4 md:pb-6`}>
           {poem?.title}
         </p>
         <div className="overflow-y-scroll text-wrap w-full pb-4">
@@ -106,14 +106,14 @@ function App() {
             return (
               <div>
                 {i % 4 == 0 && i > 0 ? (
-                  <p className="px-4 md:px-6 dancing-script border-b-1 border-solid border-b-[#A58D7F] text-lg text-transparent">
+                  <p className={`${c.charCodeAt(0) > 255 ? "baybayin" : "dancing-script"} px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg text-transparent`}>
                     .{" "}
                   </p>
                 ) : (
                   ""
                 )}
                 <p
-                  className={`px-4 md:px-6 dancing-script border-b-1 border-solid border-b-[#A58D7F] text-lg`}
+                  className={`${c.charCodeAt(0) > 255 ? "baybayin italic" : "dancing-script"} px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg`}
                 >
                   {c}
                 </p>
