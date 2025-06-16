ee5075346ecd978e17d21c3c1bb2a7f8a8a2936f
RyannKim327 <rksesgundo123@gmail.com>
2025-06-16 23:39:02 +0800

    06-16-25 23:39 - @RyannKim327 [Changed design]

--- src/App.tsx
+++ src/App.tsx
@@ -23,15 +23,15 @@ function App() {
   }, [poems]);
 
   return (
-    <div className="flex h-dvh w-dvw bg-[#FFF1E6] text-[#5C3A21] box-border select-none">
-      <div className="flex flex-col w-1/3 bg-[#FFF1E6] md:w-3/12 gap-1 p-2 box-border">
+    <div className="flex h-dvh w-dvw bg-[#F5ECD9] text-[#2e2e2e] box-border select-none">
+      <div className="flex flex-col w-1/3 bg-[#A58D7F] text-[#3B2F2F] md:w-3/12 gap-1 p-2 box-border">
         {/* TODO: Poem list */}
-        <p className="bg-[#F2D7D9] text-center p-[3px] rounded cursor-pointer border-[#C3BABA] border-2 border-solid">
+        <p className="bg-[#A58D7F] text-center p-[3px] rounded cursor-pointer border-[#3B2F2F] border-2 border-solid">
           Poems List <span className="text-[10px]">[{poems.length}]</span>
         </p>
         <div className="flex flex-col overflow-y-scroll gap-1">
           {loading ? (
-            <p className="bg-[#F2D7D9] p-[3px] rounded cursor-pointer border-[#C3BABA] border-2 border-solid">
+            <p className="bg-[#A58D7F] p-[3px] rounded cursor-pointer border-[#3B2F2F] border-2 border-solid">
               Please Wait
             </p>
           ) : (
@@ -43,7 +43,7 @@ function App() {
                 onClick={() => {
                   setPoem(poems[i]);
                 }}
-                className="bg-[#F2D7D9] p-[3px] rounded cursor-pointer border-[#C3BABA] border-2 border-solid"
+                className="bg-[#A58D7F] p-[3px] rounded cursor-pointer border-[#3B2F2F] border-2 border-solid"
               >
                 {p.title}
               </div>
@@ -53,15 +53,15 @@ function App() {
           })}
         </div>
       </div>
-      <div className="flex flex-col border-[#C3BABA] border-2 border-solid rounded justify-start items-center w-full bg-[#F2D7D9] m-2 box-border">
-        <p className="text-[#C49991] text-lg md:text-5xl font-serif">
+      <div className="flex flex-col border-[#3B2F2F] border-2 border-solid rounded justify-start items-center w-full bg-#A58D7F m-2 box-border">
+        <p className="text-[#3B2F2F] text-lg md:text-5xl font-serif">
           {poem?.title}
         </p>
         <p className="overflow-y-scroll text-wrap w-full px-2 pb-4 md:px-6">
           {poem?.content.map((c, i) => {
             return (
               <p
-                className={`${i % 4 == 0 && poem.content.length > 5 ? "mt-4 md:mt-10" : ""}`}
+                className={`${i % 4 == 0 && poem.content.length > 5 ? "mt-4 md:mt-10" : ""} px-2 dancing-script border-b-[1px] border-solid border-b-[#A58D7F] text-lg`}
               >
                 {c}
               </p>
