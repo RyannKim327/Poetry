6a6d761f20da10765a7ff99d146903a6bcaa3ba6
RyannKim327 <rksesgundo123@gmail.com>
2025-06-18 05:37:16 +0800

    06-18-25  5:37 - @RyannKim327 [Make it responsive]

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [sort, setSort] = useState("");
   const [defaultList, setDefaultList] = useState<Poems[]>([]);
+  const [selected, setSelected] = useState<boolean>(true);
 
   useEffect(() => {
     (async () => {
@@ -28,8 +29,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    document.title = poem?.title || ""
-  }, [poem])
+    document.title = poem?.title || "";
+  }, [poem]);
 
   useEffect(() => {
     setPoem(poems[0]);
@@ -68,7 +69,9 @@ function App() {
 
   return (
     <div className="flex h-dvh w-dvw bg-[#F5ECD9] text-[#2e2e2e] box-border select-none">
-      <div className="flex flex-col w-1/3 bg-[#F5ECD9] text-[#3B2F2F] md:w-3/12 gap-1 p-2 box-border">
+      <div
+        className={`${selected ? "flex z-10 md:z-0" : "hidden"} md:flex flex-col w-1/2 absolute md:static md:w-1/3 bg-[#F5ECD9] text-[#3B2F2F] md:w-3/12 gap-1 p-2 box-border`}
+      >
         {/* TODO: Poem list */}
         <p
           onClick={() => setSortList()}
@@ -92,6 +95,7 @@ function App() {
               <div
                 onClick={() => {
                   setPoem(poems[i]);
+                  setSelected(false);
                 }}
                 className={`${p.title.charCodeAt(0) > 255 ? "baybayin" : "font-serif"} bg-[#F5ECD9] p-[3px] rounded cursor-pointer border-[#3B2F2F] border-1 border-solid`}
               >
@@ -103,7 +107,7 @@ function App() {
           })}
         </div>
       </div>
-      <Poem poem={poem} />
+      <Poem setSelected={setSelected} poem={poem} />
     </div>
   );
 }
--- src/components/poem.tsx
+++ src/components/poem.tsx
@@ -1,36 +1,54 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { Poems } from "../types";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons/faArrowLeft";
+import type React from "react";
 
 interface props {
-    poem: Poems | undefined
+  setSelected: React.Dispatch<React.SetStateAction<boolean>>;
+  poem: Poems | undefined;
 }
 
-export default function Poem(props: props){
-    const title = props.poem?.title ?? "a"
-    return (
-        <div className="flex flex-col border-[#3B2F2F] rounded justify-start items-center w-full bg-[#F5ECD9] m-2 box-border shadow-[0_0_10px_#A58D7F]">
-        <p className={`${title.charCodeAt(0) > 255 ? "baybayin" : "font-serif"} text-[#3B2F2F] text-lg md:text-5xl pb-4 md:pb-6`}>
+export default function Poem(props: props) {
+  const title = props.poem?.title ?? "a";
+  return (
+    <div className="flex flex-col border-[#3B2F2F] rounded justify-start items-center w-full bg-[#F5ECD9] m-2 box-border shadow-[0_0_10px_#A58D7F]">
+      <div className="flex w-full items-center px-2">
+        <FontAwesomeIcon
+          onClick={() => {
+            props.setSelected(true);
+          }}
+          className="pb-4 inline-block md:hidden md:w-[0px]"
+          icon={faArrowLeft}
+        />
+
+        <p
+          className={`${title.charCodeAt(0) > 255 ? "baybayin" : "font-serif"} w-full text-center text-[#3B2F2F] text-lg md:text-5xl pb-4 md:pb-6`}
+        >
           {props.poem?.title}
         </p>
-        <div className="overflow-y-scroll text-wrap w-full pb-4">
-          {props.poem?.content.map((c, i) => {
-            return (
-              <div>
-                {i % 4 == 0 && i > 0 ? (
-                  <p className={`px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg text-transparent`}>
-                    Fault Line
-                  </p>
-                ) : (
-                  ""
-                )}
-                <p
-                  className={`${c.charCodeAt(0) > 255 ? "baybayin italic" : "dancing-script"} px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg lg:text-2xl`}
-                >
-                  {c}
-                </p>
-              </div>
-            );
-          })}
-        </div>
       </div>
-    )
+      <div className="overflow-y-scroll text-wrap w-full pb-4">
+        {props.poem?.content.map((c, i) => {
+          return (
+            <div>
+              {i % 4 == 0 && i > 0 ? (
+                <p
+                  className={`px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg text-transparent`}
+                >
+                  Fault Line
+                </p>
+              ) : (
+                ""
+              )}
+              <p
+                className={`${c.charCodeAt(0) > 255 ? "baybayin italic" : "dancing-script"} px-4 md:px-6 border-b-1 border-solid border-b-[#A58D7F] text-lg lg:text-2xl`}
+              >
+                {c}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
